0b5a33d98f626ed43bdc03201f93726fab6f8663
samadhigunasinghe <samadhilakmina.sl@gmail.com>
2023-02-06 08:14:22 +0530

    Update 7

--- Probe/loading.cs
+++ Probe/loading.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using System.Windows.Forms;
 
 namespace Probe
@@ -16,10 +9,10 @@ namespace Probe
         {
             InitializeComponent();
 
-            
+
             label9.Parent = pictureBox2;
             label9.BackColor = Color.Transparent;
-            pictureBox1.Parent= pictureBox2;
+            pictureBox1.Parent = pictureBox2;
 
         }
 
